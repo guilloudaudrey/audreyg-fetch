3d38f894b4e77dabbc4a258c8ecf987b1bde7870
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-08-22 10:25:30 +0200

    init

---
+++ fetch.js
@@ -0,0 +1,19 @@
+       module.exports.fetch = fetch;
+
+       function fetch(url) {
+           return new Promise(function(ok, ko) {
+               let xhr = new XMLHttpRequest();
+               xhr.open("GET", url);
+               xhr.onload = function() {
+                   if (xhr.status == 200) {
+                       ok(xhr.response);
+                   } else {
+                       ko(xhr.status)
+                   }
+               };
+               xhr.onerror = function(error) {
+                   ko(error);
+               }
+               xhr.send();
+           });
+       }
